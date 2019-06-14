dd4fb601303feb9976c83a5d1490d7888e4cd3d0
Jonatas Alves <Jonatas Alves@10.253.31.75>
2019-06-13 21:03:59 -0300

    commint from UEM Relatorios

--- public_html/js/charts.js
+++ public_html/js/charts.js
@@ -7,21 +7,48 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
   ['Task', 'Hours per Day'],
-  ['Work', 8],
-  ['Friends', 2],
-  ['Eat', 2],
-  ['TV', 2],
-  ['Gym', 2],
-  ['Sleep', 8]
+  ['Nome', 8],
+  ['Fornecedor', 2],
+  ['Valor', 2],
+  ['Data', 2]
 ]);
 
   // Optional; add a title and set the width and height of the chart
-  var options = {'title':'My Average Day', 'width':550, 'height':400, is3D: true};
+  var options = {'title':'Estatisticas:', 'width':550, 'height':400, is3D: true};
 
   // Display the chart inside the <div> element with id="piechart"
   var chart = new google.visualization.PieChart(document.getElementById('piechart'));
   chart.draw(data, options);
 }
 
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawBasic);
 
+function drawBasic() {
+        var data = google.visualization.arrayToDataTable([
+        ["Element", "Densidade", { role: "style" } ],
+        ["Nome", 8.94, "#b87333"],
+        ["Fornecedor", 10.49, "silver"],
+        ["Valor", 19.30, "gold"],
+        ["Data", 21.45, "color: #e5e4e2"]
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Estatisticas",
+        width: 550,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("chart_div"));
+      chart.draw(view, options);
+    }
 
